0fb65ae20e7323b9acd8b2a9a4ef58270b943523
NAM-IL <newlaser@hanmail.net>
2017-07-31 03:52:08 +0900

    test

--- js/app.js
+++ js/app.js
@@ -6,13 +6,13 @@ var data = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100];
 
 d3.select("body").append("div").text("새로운 문장");
 
-//d3.select("body").selectAll("div")
-//.data(data)
-//.enter()
-//.append("div");
-//.style("height", function (d) { // 높이 설정
-//  return d + "px";
-//})
-//.style("width", function (d) { // 너비 설정
-//  return "20px";
-//});
+d3.select("body").selectAll("div")
+.data(data)
+.enter()
+.append("div");
+.style("height", function (d) { // 높이 설정
+  return d + "px";
+})
+.style("width", function (d) { // 너비 설정
+  return "20px";
+});
