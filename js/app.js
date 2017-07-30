95a75088609bbe4eaa9e270a0567453dc5d737af
NAM-IL <newlaser@hanmail.net>
2017-07-31 04:06:14 +0900

    test

--- js/app.js
+++ js/app.js
@@ -22,3 +22,44 @@ d3.select("body").selectAll("div")
   return "20px";
 })
 .attr("class", "bar-chart");
+
+
+svg.selectAll("rect")
+.data(data)
+.enter()
+.append("rect");
+
+
+svg.attr("x", function(d, i) { // x 좌표 설정
+      return i * (w / data.length)
+    })
+    .attr("y", function(d) { // y 좌표 설정
+      return h - d;
+    })
+    .attr("width", w / data.length - 1) // 너비 설정
+    .attr("height", function(d) { // 높이 설정
+      return d;
+    })
+    .attr("fill", function(d) { // 색상 설정
+      return "hotpink";
+    });
+
+
+svg.selectAll("text")
+.data(data)
+.enter()
+.append("text")
+.text(function(d) {
+  return d;
+})
+.attr("x", function(d, i) {
+  return i * (w / data.length) + (w / data.length) / 2;
+})
+.attr("y", function(d) {
+  return h - d + 10;
+})
+.attr("font-family", "sans-serif")
+.attr("font-size", "11px")
+.attr("fill", "black")
+.attr("text-anchor", "middle");
+
